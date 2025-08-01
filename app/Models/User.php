295e098e0a124a97d5dd32279179199dee6b9bbb
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-08-01 17:38:01 -0300

    feat(user): adding policy attribute to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Policies\UserPolicy;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
+use Illuminate\Database\Eloquent\Attributes\UsePolicy;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+#[UsePolicy(UserPolicy::class)]
 final class User extends Authenticatable implements FilamentUser
 {
     use HasFactory;
