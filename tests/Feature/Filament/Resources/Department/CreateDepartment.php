bb43a91624ee15f8e4ad7261758d31a628301f31
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-08-04 19:44:53 -0300

    test(CreateDepartment): department manager should be added to users relationship after creating a department

--- tests/Feature/Filament/Resources/Department/CreateDepartment.php
+++ tests/Feature/Filament/Resources/Department/CreateDepartment.php
@@ -33,3 +33,30 @@ it('should be able to create a department', function (): void {
     expect($department->manager->getKey())
         ->toBe($manager->getKey());
 });
+
+test('department manager should be added to users relationship after creating a department', function (): void {
+    $employees = User::factory()->count(10)->create();
+    $company = Company::factory()->create();
+    $manager = User::factory()->create();
+
+    actingAs($manager);
+
+    livewire(CreateDepartment::class)
+        ->fillForm([
+            'budget' => 5000,
+            'name' => 'Department name',
+            'slug' => 'department-name',
+            'manager_id' => $manager->id,
+            'company_id' => $company->id,
+            'users' => $employees->pluck('id')->toArray(),
+        ])
+        ->call('create')
+        ->assertHasNoErrors();
+
+    $department = Department::query()->first();
+    expect($department->manager->getKey())
+        ->toBe($manager->getKey())
+        ->and($department->users()->count())
+        ->toBe($employees->count() + 1)
+        ->and($department->users()->where('users.id', $manager->id)->exists())->toBeTrue();
+});
