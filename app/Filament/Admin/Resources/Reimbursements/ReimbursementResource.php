c4bf36f81c06179b6ff26e883d723d6c045aa144
RichardGL11 <richardgreghilammenda@hotmail.com>
2025-08-11 18:43:20 -0300

    feat: changing icon for reimbursement resource

--- app/Filament/Admin/Resources/Reimbursements/ReimbursementResource.php
+++ app/Filament/Admin/Resources/Reimbursements/ReimbursementResource.php
@@ -15,12 +15,15 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 final class ReimbursementResource extends Resource
 {
     protected static ?string $model = Reimbursement::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::Ticket;
+
+    protected static string|UnitEnum|null $navigationGroup = 'Financial';
 
     public static function form(Schema $schema): Schema
     {
